880e0e01bbd2925e766d7a0ef3b16d7e9d48e036
sfatbro786 <solaimanislamsifat@gmail.com>
2025-10-23 15:12:05 +0600

    completed webhook functionality

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,44 @@
+export const dynamic = "force-dynamic";
+
+import Transaction from "@/models/transaction";
+import dbConnect from "@/utils/db";
+import stripe from "@/utils/stripe";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    await dbConnect();
+
+    const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+    const sig = req.headers.get("stripe-signature")!;
+    const body = await req.text();
+
+    try {
+        // verify the webhook signature and parse the event
+        const event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+
+        if (event.type === "checkout.session.completed") {
+            const session = event.data.object as any;
+
+            const transaction = await new Transaction({
+                sessionId: session.id,
+                customerId: session.customer,
+                invoiceId: session.invoice,
+                subscriptionId: session.subscription,
+                mode: session.mode,
+                paymentStatus: session.payment_status,
+                customerEmail: session.customer_email,
+                amountTotal: session.amount_total,
+                status: session.status,
+            });
+
+            await transaction.save();
+
+            return NextResponse.json({
+                message: "Webhook received: Checkout session completed",
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        return new Response("Webhook Error", { status: 400 });
+    }
+}
